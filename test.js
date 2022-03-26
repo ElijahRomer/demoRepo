02255e907bfd5b282db69a0dc9b0c0a63f6e0eef
Elijah Romer <romere88@gmail.com>
2022-03-26 10:29:43 -0400

    I have added 3 test files.

---
+++ test.js
@@ -0,0 +1,19 @@
+function whatup() {
+  console.log(`Whatup`);
+};
+
+
+
+console.log(typeof whatup)
+
+document.addEventListener("click", isItChecked)
+
+function isItChecked() {
+  let checkbox = document.getElementById("testCheckBox").checked;
+
+  console.log(checkbox);
+}
+
+$('#div1').hide();
+
+console.log(div1);
